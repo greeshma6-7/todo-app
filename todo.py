2135550ef8210d2c958934300c97b23ca7a19251
greeshu12 <greeshmachowdary98@gmail.com>
2025-12-11 04:53:59 +0000

    feat: add clear command to todo app

--- todo.py
+++ todo.py
@@ -1,23 +1,3 @@
-tasks = []
-
-def add(task):
-    tasks.append(task)
-    print("Task added:", task)
-
-def list_tasks():
-    if not tasks:
-        print("No tasks found.")
-    else:
-        for i, t in enumerate(tasks, start=1):
-            print(f"{i}. {t}")
-
-def done(num):
-    try:
-        removed = tasks.pop(num - 1)
-        print("Completed:", removed)
-    except IndexError:
-        print("Invalid task number.")
-
 if __name__ == "__main__":
     import sys
     if len(sys.argv) < 2:
@@ -32,5 +12,8 @@ if __name__ == "__main__":
         list_tasks()
     elif cmd == "done":
         done(int(sys.argv[2]))
+    elif cmd == "clear":
+        tasks.clear()
+        print("All tasks cleared.")
     else:
         print("Unknown command")
