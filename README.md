8517fe754f0f7c45e33e34ac22194c234b848b7c
greeshu12 <greeshmachowdary98@gmail.com>
2025-12-11 04:25:11 +0000

    Initial commit: todo app

---
+++ README.md
@@ -0,0 +1,7 @@
+# Todo App (CLI)
+
+A simple command-line todo application.
+Supports:
+- Add tasks
+- List tasks
+- Mark tasks as done
---
+++ todo.py
@@ -0,0 +1,36 @@
+tasks = []
+
+def add(task):
+    tasks.append(task)
+    print("Task added:", task)
+
+def list_tasks():
+    if not tasks:
+        print("No tasks found.")
+    else:
+        for i, t in enumerate(tasks, start=1):
+            print(f"{i}. {t}")
+
+def done(num):
+    try:
+        removed = tasks.pop(num - 1)
+        print("Completed:", removed)
+    except IndexError:
+        print("Invalid task number.")
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("Usage: python todo.py [add/list/done] <task>")
+        exit()
+
+    cmd = sys.argv[1]
+
+    if cmd == "add":
+        add(" ".join(sys.argv[2:]))
+    elif cmd == "list":
+        list_tasks()
+    elif cmd == "done":
+        done(int(sys.argv[2]))
+    else:
+        print("Unknown command")
